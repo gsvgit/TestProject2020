a88074ac63aac076175c751a13a6a0137fe99cac
gsv <rsdpisuy@gmail.com>
2020-09-11 14:17:39 +0300

    Initial commit.

---
+++ tests/TestProj2020.Tests/Tests.fs
@@ -0,0 +1,15 @@
+module Tests
+
+
+open Expecto
+open TestProj2020
+
+[<Tests>]
+let tests =
+    testList "samples"
+        [ testCase "Say nothing" <| fun _ ->
+            let subject = Say.nothing()
+            Expect.equal subject () "Not an absolute unit"
+          testCase "Say hello all" <| fun _ ->
+              let subject = Say.hello "all"
+              Expect.equal subject "Hello all" "You didn't say hello" ]
