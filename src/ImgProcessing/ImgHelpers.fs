90ccce8d9957b9e44483360e24bc3129c727fe62
gsv <rsdpisuy@gmail.com>
2021-03-02 16:13:37 +0300

    GPGPU version of image processing.

--- src/ImgProcessing/ImgHelpers.fs
+++ src/ImgProcessing/ImgHelpers.fs
@@ -1,6 +1,11 @@
 module TestProj2020.ImgHelpers
 open System.Drawing
-open System.IO
+open Brahma.FSharp.OpenCL.Translator.Common
+open OpenCL.Net
+open Brahma.OpenCL
+open Brahma.FSharp.OpenCL.Core
+open Microsoft.FSharp.Quotations
+open Brahma.FSharp.OpenCL.Extensions
 
 let loadAs2DArray (file:string) =
     let img = new Bitmap(file)
@@ -34,7 +39,7 @@ let gaussianBlurKernel =
       [| 4; 16; 24; 16; 4|]
       [| 1; 4;  6;  4;  1|]
     |]
-    |> Array.map (fun a -> Array.map (fun x -> (float x) / 256.0) a)
+    |> Array.map (fun a -> Array.map (fun x -> (float32 x) / 256.0f) a)
 
 let edgesKernel =
     [|
@@ -44,7 +49,7 @@ let edgesKernel =
       [|0;  0;  0;  0;  0|]
       [|0;  0;  0;  0;  0|]
     |]
-    |> Array.map (fun x -> Array.map float x)
+    |> Array.map (fun x -> Array.map float32 x)
 
 let applyFilter (filter: 't[][]) (img: byte[,]) =
     let imgH = img.GetLength 0
@@ -121,3 +126,134 @@ let applyFilter3 (filter: 't[][]) (img: byte[,]) =
 
     result
 
+let getCommandQueue platformName =
+    let deviceType = DeviceType.Default
+
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with
+        | ex -> failwith ex.Message
+
+    let mutable commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)
+
+    provider, commandQueue
+
+let applyFilter4 (provider:ComputeProvider) (commandQueue:CommandQueue) localWorkSize (filter: float32[][]) (img: byte[,]) =
+    let imgH = img.GetLength 0
+    let imgW = img.GetLength 1
+    let img =
+            [| for x in 0 .. Array2D.length1 img - 1 do
+               yield! [| for y in 0 .. Array2D.length2 img - 1 -> img.[x, y] |]
+            |]
+    let filterD = (Array.length filter) / 2
+    let filterSize = filter.Length * filter.Length
+
+    let result = Array2D.zeroCreate imgH imgW
+
+    let command =
+        <@
+            fun (r:_1D) (img:array<_>) (filter:array<_>) (result:array<_>) ->
+                let p = r.GlobalID0
+                let localId = r.LocalID0
+
+                let pw = p % imgW
+                let ph = p / imgW
+
+                let localFilter = localArray filterSize
+
+                if localId < filterSize then localFilter.[localId] <- filter.[localId]
+
+                barrier()
+
+                let mutable res = 0.0f
+
+                for i in ph - filterD .. ph + filterD do
+                    for j in pw - filterD .. pw + filterD do
+                        let mutable d = 0uy
+                        if i < 0 || i >= imgH || j < 0 || j >= imgW
+                        then d <- img.[p]
+                        else d <- img.[i * imgW + j]
+                        let f = localFilter.[(i - ph + filterD) * (2 * filterD + 1) + (j - pw + filterD)]
+                        res <- res + (float32 d) * f
+                result.[p] <- res
+        @>
+
+    let str = ref ""
+    let kernel, kernelPrepare, kernelRun = provider.Compile command //(command, _options = CompileOptions.DisableOptimizations, _outCode = str, translatorOptions=[TranslatorOption.BoolAsBit])
+    let d = _1D(imgH * imgW, localWorkSize)
+    let result' = Array.zeroCreate (imgH * imgW)
+    let filter = Array.concat filter
+
+    kernelPrepare d img filter result'
+
+    commandQueue.Add(kernelRun()) |> ignore
+    let _ = commandQueue.Add(result'.ToHost provider).Finish()
+
+    Array.Parallel.iteri (fun x v -> result.[x / imgW, x % imgW] <- byte v) result'
+
+    provider.CloseAllBuffers()
+
+    result
+
+
+let applyFilters (provider:ComputeProvider) (commandQueue:CommandQueue) localWorkSize (filters: list<float32[][]>) (img: byte[,]) =
+    let imgH = img.GetLength 0
+    let imgW = img.GetLength 1
+    let img =
+            [| for x in 0 .. Array2D.length1 img - 1 do
+               yield! [| for y in 0 .. Array2D.length2 img - 1 -> img.[x, y] |]
+            |]
+
+    let applyFilter img (filter:'t[][]) =
+        let filterD = (Array.length filter) / 2
+        let filter = Array.concat filter
+
+        let filterSize = filter.Length
+
+        let command =
+            <@
+                fun (r:_1D) (img:array<_>) (filter:array<_>) (result:array<_>) ->
+                    let p = r.GlobalID0
+                    //let localId = r.LocalID0
+
+                    let pw = p % imgW
+                    let ph = p / imgW
+
+                    //let localFilter = localArray filterSize
+
+                    //if localId < filterSize then localFilter.[localId] <- filter.[localId]
+
+                    //barrier()
+
+                    let mutable res = 0.0f
+
+                    for i in ph - filterD .. ph + filterD do
+                        for j in pw - filterD .. pw + filterD do
+                            let mutable d = 0uy
+                            if i < 0 || i >= imgH || j < 0 || j >= imgW
+                            then d <- img.[p]
+                            else d <- img.[i * imgW + j]
+                            let f = filter.[(i - ph + filterD) * (2 * filterD + 1) + (j - pw + filterD)]
+                            res <- res + (float32 d) * f
+                    result.[p] <-  res
+            @>
+
+        let kernel, kernelPrepare, kernelRun = provider.Compile command
+        let d = _1D(imgH * imgW, localWorkSize)
+        let result' = Array.zeroCreate (imgH * imgW)
+        kernelPrepare d img filter result'
+
+        commandQueue.Add(kernelRun()) |> ignore
+        let _ = commandQueue.Add(result'.ToHost provider).Finish()
+        provider.CloseAllBuffers()
+        result'
+
+    let mutable res = img
+    for filter in filters do
+        res <- applyFilter res filter |> Array.Parallel.map (byte)
+    let result = Array2D.zeroCreate imgH imgW
+    Array.Parallel.iteri (fun x v -> result.[x / imgW, x % imgW] <- byte v) res
+
+    provider.CloseAllBuffers()
+
+    result
--- src/ImgProcessing/Main.fs
+++ src/ImgProcessing/Main.fs
@@ -23,17 +23,27 @@ module Main =
         let img1 = "../../../Examples/sara-budhwani-h_P71-B8BPw-unsplash.jpg"
         let img2 = "../../../Examples/nick-gavrilov-F-rvSJl6qI0-unsplash.jpg"
 
-        let bArray = ImgHelpers.loadAs2DArray img1
+        let bArray = ImgHelpers.loadAs2DArray img2
         let grey = ImgHelpers.toGrayscale bArray
 
+        let platform, queue = ImgHelpers.getCommandQueue "*NVIDIA*"
+
+        let filter4 = ImgHelpers.applyFilter4 platform queue 64
+
+        let filter =
+            filter4
+            //ImgHelpers.applyFilter3
+        let r1 () = ImgHelpers.applyFilters platform queue 64 (List.init 10 (fun i -> ImgHelpers.gaussianBlurKernel) @ [ImgHelpers.edgesKernel]) grey
         let r () =
-            ImgHelpers.applyFilter3 ImgHelpers.gaussianBlurKernel grey
-            |> ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel
-            |> ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel
-            |> ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel
-            |> ImgHelpers.applyFilter3 ImgHelpers.edgesKernel
-            //|> ImgHelpers.applyFilter ImgHelpers.gaussianBlurKernel
-        let r,t = time r
+            grey
+            |> filter ImgHelpers.gaussianBlurKernel
+            |> filter ImgHelpers.gaussianBlurKernel
+            |> filter ImgHelpers.gaussianBlurKernel
+            |> filter ImgHelpers.gaussianBlurKernel
+            |> filter ImgHelpers.edgesKernel
+
+        //let r,t = time r
+        let r,t = time r1
         printfn "Execution time: %A seconds" t
-        ImgHelpers.save2DByteArrayAsImage r (img1 + "_out_blur_edg_4.jpg")
+        ImgHelpers.save2DByteArrayAsImage r (img2 + "_out_blur_edg_4_gpu_2.jpg")
         0
