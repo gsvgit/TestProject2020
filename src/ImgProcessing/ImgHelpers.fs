079f4f9dc4388bad651f61621c59545b174cc235
gsv <rsdpisuy@gmail.com>
2021-03-02 13:20:52 +0300

    Img processing. Imperative parallel version.

--- src/ImgProcessing/ImgHelpers.fs
+++ src/ImgProcessing/ImgHelpers.fs
@@ -62,7 +62,7 @@ let applyFilter (filter: 't[][]) (img: byte[,]) =
             |]
         Array.fold2 (fun s x y -> s + x * y) 0.0 filter dataToHandle
 
-    Array2D.mapi (fun x y v -> byte (processPixel x y)) img
+    Array2D.mapi (fun x y _ -> byte (processPixel x y)) img
 
 let applyFilter2 (filter: 't[][]) (img: byte[,]) =
     let imgH = img.GetLength 0
@@ -85,5 +85,39 @@ let applyFilter2 (filter: 't[][]) (img: byte[,]) =
         Array.fold2 (fun s x y -> (s + x * y)) 0.0 filter dataToHandle
         |> byte
 
-    Array.Parallel.mapi (fun x l -> Array.Parallel.mapi (fun y _ ->  processPixel x y) l) img
+    Array.Parallel.mapi (fun x l -> Array.mapi (fun y _ ->  processPixel x y) l) img
     |> array2D
+
+
+let applyFilter3 (filter: 't[][]) (img: byte[,]) =
+    let imgH = img.GetLength 0
+    let imgW = img.GetLength 1
+    let img =
+            [| for x in 0 .. Array2D.length1 img - 1 do
+               yield! [| for y in 0 .. Array2D.length2 img - 1 -> img.[x, y] |]
+            |]
+    let filterD = (Array.length filter) / 2
+    let filter = Array.concat filter
+
+    let result = Array2D.zeroCreate imgH imgW
+
+    let processPixel p =
+        let pw = p % imgW
+        let ph = p / imgW
+
+        let mutable res = 0.0
+
+        for i in ph - filterD .. ph + filterD do
+            for j in pw - filterD .. pw + filterD do
+                let d =
+                    if i < 0 || i >= imgH || j < 0 || j >= imgW
+                    then float img.[p]
+                    else float img.[i * imgW + j]
+                let f = filter.[(i - ph + filterD) * (2 * filterD + 1) + (j - pw + filterD)]
+                res <- res + d * f
+        byte res
+
+    Array.Parallel.iteri (fun x v -> result.[x / imgW, x % imgW] <- processPixel x) img
+
+    result
+
--- src/ImgProcessing/Main.fs
+++ src/ImgProcessing/Main.fs
@@ -12,19 +12,28 @@ module Main =
                 match s with
                 | TaskPow _ -> "Run pow function"
 
+    let time f =
+        let start = System.DateTime.Now
+        let r = f ()
+        let final = (System.DateTime.Now - start).TotalMilliseconds / 1000.0
+        r,final
+
     [<EntryPoint>]
     let main (argv: string array) =
         let img1 = "../../../Examples/sara-budhwani-h_P71-B8BPw-unsplash.jpg"
         let img2 = "../../../Examples/nick-gavrilov-F-rvSJl6qI0-unsplash.jpg"
 
-        let bArray = ImgHelpers.loadAs2DArray img2
+        let bArray = ImgHelpers.loadAs2DArray img1
         let grey = ImgHelpers.toGrayscale bArray
-        let r =
-            ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel grey
+
+        let r () =
+            ImgHelpers.applyFilter3 ImgHelpers.gaussianBlurKernel grey
             |> ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel
             |> ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel
             |> ImgHelpers.applyFilter2 ImgHelpers.gaussianBlurKernel
-            |> ImgHelpers.applyFilter2 ImgHelpers.edgesKernel
+            |> ImgHelpers.applyFilter3 ImgHelpers.edgesKernel
             //|> ImgHelpers.applyFilter ImgHelpers.gaussianBlurKernel
-        ImgHelpers.save2DByteArrayAsImage r (img2 + "out_3.jpg")
+        let r,t = time r
+        printfn "Execution time: %A seconds" t
+        ImgHelpers.save2DByteArrayAsImage r (img1 + "_out_blur_edg_4.jpg")
         0
